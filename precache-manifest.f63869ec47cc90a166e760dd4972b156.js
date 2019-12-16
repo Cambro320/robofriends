c1d6946159644d1c64a740cc12ae32d9261f36f0
Jory <46983170+Cambro320@users.noreply.github.com>
2019-12-16 11:08:20 -0600

    Updates

---
+++ precache-manifest.f63869ec47cc90a166e760dd4972b156.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd3d3e84d165758a735c99b32b61051",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "23974d1df3f26e5b2b03",
+    "url": "/robofriends/static/css/main.441d545a.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23974d1df3f26e5b2b03",
+    "url": "/robofriends/static/js/main.709dfa55.chunk.js"
+  },
+  {
+    "revision": "1e6fdd492adfb4acd267",
+    "url": "/robofriends/static/js/runtime-main.2c4b189a.js"
+  },
+  {
+    "revision": "f359094b1bb10bac9417b4445bc792ac",
+    "url": "/robofriends/static/media/Starjout.f359094b.woff"
+  }
+]);
